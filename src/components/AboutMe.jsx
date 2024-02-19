af2bee04f6c44d4df0ee5c4bd5056f74b90697b9
Mario Mitic <mariomitic@yahoo.com>
2024-02-19 15:41:55 -0800

    Resume corrected

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
         <div className="nameTitle">
           <h2>About me</h2>
           <p style={{ width: "70%"}}>
-          I am a passionate and motivated entry-level front end developer with knowledge of HTML, CSS, JavaScript, and React seeking an opportunity to contribute my coding skills and creativity to a dynamic web development team. 
+          I am a passionate and motivated front end developer with knowledge of HTML, CSS, JavaScript, and React seeking an opportunity to contribute my coding skills and creativity to a dynamic web development team. 
           </p>
         </div>
   
